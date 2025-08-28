ef0f5ad404e410d6bd45f356173d1ced9f875703
project-x-link <projectxbuildx01@gmail.com>
2025-08-29 01:56:23 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Project from "../models/Project.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
